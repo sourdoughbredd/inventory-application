8414c93f5088f13feb43a05634a6365d10e4471f
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-09 11:17:35 -0400

    Implement POST new brewery from form

--- controllers/breweryController.js
+++ controllers/breweryController.js
@@ -1,6 +1,7 @@
 const Brewery = require("../models/brewery");
 const Beer = require("../models/beer");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all breweries
 exports.brewery_list = asyncHandler(async (req, res, next) => {
@@ -37,9 +38,75 @@ exports.brewery_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle Brewery create on POST
-exports.brewery_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Brewery create POST");
-});
+exports.brewery_create_post = [
+  // Validate and sanitize the request
+  body("name", "Name must be at least 2 characters.")
+    .trim()
+    .isLength({ min: 2 }),
+  body("city", "City must be at least 2 characters.")
+    .optional({ values: "falsy" })
+    .trim()
+    .isLength({ min: 2 }),
+  body("state", "State must be at least 2 characters.")
+    .optional({ values: "falsy" })
+    .trim()
+    .isLength({ min: 2 }),
+  body("country", "Country must be at least 4 characters.")
+    .optional({ values: "falsy" })
+    .trim()
+    .isLength({ min: 4 }),
+  body("longitude_deg", "Longitude must be a number between -180 and 180.")
+    .optional({ values: "falsy" })
+    .trim()
+    .isNumeric()
+    .toFloat()
+    .custom((val) => val >= -180 && val <= 180),
+  body("latitude_deg", "Latitude must be a number between -90 and 90.")
+    .optional({ values: "falsy" })
+    .trim()
+    .isNumeric()
+    .toFloat()
+    .custom((val) => val >= -90 && val <= 90),
+
+  // Process the request
+  asyncHandler(async (req, res, next) => {
+    // Extract any validation errors
+    const errors = validationResult(req);
+
+    // Create a new brewery object
+    const brewery = new Brewery({
+      name: req.body.name,
+      city: req.body.city,
+      state: req.body.state,
+      country: req.body.country,
+      location: {
+        type: "Point",
+        coordinates: [req.body.latitude_deg, req.body.longitude_deg],
+      },
+    });
+
+    // Check for errors before continuing
+    if (!errors.isEmpty()) {
+      res.render("brewery_form", {
+        title: "Add New Brewery",
+        brewery: brewery,
+        errors: errors.array(),
+      });
+    } else {
+      // Check if Brewery exists already before saving
+      const existingBrewery = await Brewery.findOne({
+        name: brewery.name,
+      }).exec();
+
+      if (existingBrewery) {
+        res.redirect(existingBrewery.url);
+      } else {
+        await brewery.save();
+        res.redirect(brewery.url);
+      }
+    }
+  }),
+];
 
 // Display Brewery delete form on GET
 exports.brewery_delete_get = asyncHandler(async (req, res, next) => {
