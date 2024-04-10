763c311e6db3eee63d9810e4e6d388b4b095b80f
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-10 11:28:14 -0400

    Implement POST create beer SKU form

--- controllers/beerSkuController.js
+++ controllers/beerSkuController.js
@@ -1,6 +1,10 @@
 const BeerSku = require("../models/beerSku");
 const Beer = require("../models/beer");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const beer = require("../models/beer");
+
+const OZ_TO_ML = 29.5735;
 
 // Display list of all beer SKUs
 exports.beersku_list = asyncHandler(async (req, res, next) => {
@@ -38,9 +42,83 @@ exports.beersku_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle beer SKU create on POST
-exports.beersku_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Beer SKU create POST");
-});
+exports.beersku_create_post = [
+  // Validate and sanitize
+  body("beer", "Invalid beer").isMongoId(),
+  body("packaging_type", "Packaging type").trim().isLength({ min: 1 }),
+  body("packaging_volume_oz", "Volume must be a number greater than 0.")
+    .trim()
+    .isNumeric()
+    .toFloat()
+    .custom((val) => val > 0),
+  body(
+    "packaging_quantity",
+    "Packaging quantity must be an integer greater than 0."
+  )
+    .trim()
+    .isInt({ min: 1 })
+    .toInt(),
+  body("price", "Price must be a number greater than or equal to 0.00")
+    .trim()
+    .isNumeric()
+    .toFloat()
+    .custom((val) => val >= 0),
+  body("stock", "Stock must be an integer greater than or equal to 0")
+    .trim()
+    .isInt({ min: 0 })
+    .toInt(),
+
+  // Process request
+  asyncHandler(async (req, res, next) => {
+    // Extract validation results
+    const errors = validationResult(req);
+
+    // Create new beer SKU
+    const beerSku = new BeerSku({
+      beer: req.body.beer,
+      packaging: {
+        type: req.body.packaging_type,
+        volume_oz: req.body.packaging_volume_oz,
+        volume_ml: Math.round(OZ_TO_ML * req.body.packaging_volume_oz),
+        quantity: req.body.packaging_quantity,
+      },
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    // Handle any validation errors
+    if (!errors.isEmpty()) {
+      // Re-render form and show errors to user
+      const beers = await Beer.find({})
+        .collation({ locale: "en" })
+        .sort({ name: 1 })
+        .populate("brewery")
+        .exec();
+
+      res.render("beersku_form", {
+        title: "Create New Beer SKU",
+        beers,
+        beersku: beerSku,
+        selected_beer: beerSku.beer._id,
+        errors: errors.array(),
+      });
+    }
+
+    // Make sure beer SKU doesn't already exist
+    const existingBeerSku = await BeerSku.findOne({
+      _id: { $ne: req.params.id },
+      beer: req.body.beer,
+      packaging: beerSku.packaging,
+    });
+
+    if (existingBeerSku) {
+      res.redirect(existingBeerSku.url);
+    } else {
+      await beerSku.save();
+      res.redirect(beerSku.url);
+    }
+  }),
+];
 
 // Display beer SKU delete form on GET
 exports.beersku_delete_get = asyncHandler(async (req, res, next) => {
