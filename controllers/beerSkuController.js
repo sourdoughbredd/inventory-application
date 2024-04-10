38235c3f00ff50c131264c59f61f789384ba9671
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-10 12:08:47 -0400

    Implement POST update beer SKU form

--- controllers/beerSkuController.js
+++ controllers/beerSkuController.js
@@ -175,6 +175,81 @@ exports.beersku_update_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle beer SKU update on POST
-exports.beersku_update_post = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Beer SKU update POST: ${req.params.id}`);
-});
+exports.beersku_update_post = [
+  // Validate and sanitize
+  body("beer", "Invalid beer").isMongoId(),
+  body("packaging_type", "Packaging type").trim().isLength({ min: 1 }),
+  body("packaging_volume_oz", "Volume must be a number greater than 0.")
+    .trim()
+    .isNumeric()
+    .toFloat()
+    .custom((val) => val > 0),
+  body(
+    "packaging_quantity",
+    "Packaging quantity must be an integer greater than 0."
+  )
+    .trim()
+    .isInt({ min: 1 })
+    .toInt(),
+  body("price", "Price must be a number greater than or equal to 0.00")
+    .trim()
+    .isNumeric()
+    .toFloat()
+    .custom((val) => val >= 0),
+  body("stock", "Stock must be an integer greater than or equal to 0")
+    .trim()
+    .isInt({ min: 0 })
+    .toInt(),
+
+  // Process request
+  asyncHandler(async (req, res, next) => {
+    // Extract validation results
+    const errors = validationResult(req);
+
+    // Create new beer SKU
+    const beerSku = new BeerSku({
+      _id: req.params.id,
+      beer: req.body.beer,
+      packaging: {
+        type: req.body.packaging_type,
+        volume_oz: req.body.packaging_volume_oz,
+        volume_ml: Math.round(OZ_TO_ML * req.body.packaging_volume_oz),
+        quantity: req.body.packaging_quantity,
+      },
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    // Handle any validation errors
+    if (!errors.isEmpty()) {
+      // Re-render form and show errors to user
+      const beers = await Beer.find({})
+        .collation({ locale: "en" })
+        .sort({ name: 1 })
+        .populate("brewery")
+        .exec();
+
+      res.render("beersku_form", {
+        title: "Update Beer SKU: " + beerSku._id,
+        beers,
+        beersku: beerSku,
+        selected_beer: beerSku.beer,
+        errors: errors.array(),
+      });
+    }
+
+    // Make sure beer SKU doesn't already exist
+    const existingBeerSku = await BeerSku.findOne({
+      _id: { $ne: req.params.id },
+      beer: req.body.beer,
+      packaging: beerSku.packaging,
+    });
+
+    if (existingBeerSku) {
+      res.redirect(existingBeerSku.url);
+    } else {
+      await BeerSku.findByIdAndUpdate(req.params.id, beerSku, {}).exec();
+      res.redirect(beerSku.url);
+    }
+  }),
+];
