21ee0262c28aec990925e511fc6c1c022f5a3411
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-15 11:56:45 -0400

    Add location endpoint routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const inventoryRouter = require("./routes/inventory");
+const locationsRouter = require("./routes/locations");
 
 const app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/inventory", inventoryRouter);
+app.use("/locations", locationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
